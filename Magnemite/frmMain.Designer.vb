1c9a6db35e320419b26b97c69f81f1f7b80e4d6e
spiriguidiberto <spiriguidiberto@gmail.com>
2016-04-28 10:10:57 -0300

    Initial commit

---
+++ Magnemite/frmMain.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits DevExpress.XtraBars.Ribbon.RibbonForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.RibbonControl = New DevExpress.XtraBars.Ribbon.RibbonControl()
+        Me.btnSeleioncaDir = New DevExpress.XtraBars.BarButtonItem()
+        Me.btnVerifica = New DevExpress.XtraBars.BarButtonItem()
+        Me.btnRegistro = New DevExpress.XtraBars.BarButtonItem()
+        Me.btnExcluir = New DevExpress.XtraBars.BarButtonItem()
+        Me.RibbonPage1 = New DevExpress.XtraBars.Ribbon.RibbonPage()
+        Me.RibbonPageGroup1 = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
+        Me.RibbonPageGroup2 = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
+        Me.RibbonStatusBar = New DevExpress.XtraBars.Ribbon.RibbonStatusBar()
+        Me.DefaultLookAndFeel1 = New DevExpress.LookAndFeel.DefaultLookAndFeel(Me.components)
+        Me.TreeList1 = New DevExpress.XtraTreeList.TreeList()
+        Me.colfileName = New DevExpress.XtraTreeList.Columns.TreeListColumn()
+        Me.colLocal = New DevExpress.XtraTreeList.Columns.TreeListColumn()
+        Me.ImageCollection1 = New DevExpress.Utils.ImageCollection(Me.components)
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        CType(Me.RibbonControl, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TreeList1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ImageCollection1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RibbonControl
+        '
+        Me.RibbonControl.AutoSizeItems = True
+        Me.RibbonControl.ColorScheme = DevExpress.XtraBars.Ribbon.RibbonControlColorScheme.Blue
+        Me.RibbonControl.ExpandCollapseItem.Id = 0
+        Me.RibbonControl.ExpandCollapseItem.Name = ""
+        Me.RibbonControl.Items.AddRange(New DevExpress.XtraBars.BarItem() {Me.RibbonControl.ExpandCollapseItem, Me.btnSeleioncaDir, Me.btnVerifica, Me.btnRegistro, Me.btnExcluir})
+        Me.RibbonControl.Location = New System.Drawing.Point(0, 0)
+        Me.RibbonControl.MaxItemId = 5
+        Me.RibbonControl.Name = "RibbonControl"
+        Me.RibbonControl.Pages.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPage() {Me.RibbonPage1})
+        Me.RibbonControl.ShowApplicationButton = DevExpress.Utils.DefaultBoolean.[False]
+        Me.RibbonControl.ShowCategoryInCaption = False
+        Me.RibbonControl.ShowExpandCollapseButton = DevExpress.Utils.DefaultBoolean.[False]
+        Me.RibbonControl.ShowToolbarCustomizeItem = False
+        Me.RibbonControl.Size = New System.Drawing.Size(442, 147)
+        Me.RibbonControl.StatusBar = Me.RibbonStatusBar
+        Me.RibbonControl.Toolbar.ShowCustomizeItem = False
+        Me.RibbonControl.ToolbarLocation = DevExpress.XtraBars.Ribbon.RibbonQuickAccessToolbarLocation.Hidden
+        '
+        'btnSeleioncaDir
+        '
+        Me.btnSeleioncaDir.Caption = "Selecionar Local"
+        Me.btnSeleioncaDir.Id = 1
+        Me.btnSeleioncaDir.LargeGlyph = Global.Magnemite.My.Resources.Resources.opened_folder_48
+        Me.btnSeleioncaDir.Name = "btnSeleioncaDir"
+        '
+        'btnVerifica
+        '
+        Me.btnVerifica.Caption = "Verificar Arquivos"
+        Me.btnVerifica.Id = 2
+        Me.btnVerifica.LargeGlyph = Global.Magnemite.My.Resources.Resources.search_property_48
+        Me.btnVerifica.Name = "btnVerifica"
+        '
+        'btnRegistro
+        '
+        Me.btnRegistro.Caption = "Adicionar ao botçao diretiro"
+        Me.btnRegistro.Id = 3
+        Me.btnRegistro.LargeGlyph = Global.Magnemite.My.Resources.Resources.mouse_right_click_48
+        Me.btnRegistro.Name = "btnRegistro"
+        '
+        'btnExcluir
+        '
+        Me.btnExcluir.Caption = "Excluir Arquivos Selecionados"
+        Me.btnExcluir.Id = 4
+        Me.btnExcluir.LargeGlyph = Global.Magnemite.My.Resources.Resources.trash_48
+        Me.btnExcluir.Name = "btnExcluir"
+        '
+        'RibbonPage1
+        '
+        Me.RibbonPage1.Groups.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageGroup() {Me.RibbonPageGroup1, Me.RibbonPageGroup2})
+        Me.RibbonPage1.Name = "RibbonPage1"
+        Me.RibbonPage1.Text = "Magnemite"
+        '
+        'RibbonPageGroup1
+        '
+        Me.RibbonPageGroup1.ItemLinks.Add(Me.btnSeleioncaDir)
+        Me.RibbonPageGroup1.ItemLinks.Add(Me.btnVerifica)
+        Me.RibbonPageGroup1.ItemLinks.Add(Me.btnExcluir)
+        Me.RibbonPageGroup1.Name = "RibbonPageGroup1"
+        Me.RibbonPageGroup1.ShowCaptionButton = False
+        Me.RibbonPageGroup1.Text = "Opções"
+        '
+        'RibbonPageGroup2
+        '
+        Me.RibbonPageGroup2.ItemLinks.Add(Me.btnRegistro)
+        Me.RibbonPageGroup2.Name = "RibbonPageGroup2"
+        Me.RibbonPageGroup2.ShowCaptionButton = False
+        Me.RibbonPageGroup2.Text = "Configurações"
+        '
+        'RibbonStatusBar
+        '
+        Me.RibbonStatusBar.Location = New System.Drawing.Point(0, 418)
+        Me.RibbonStatusBar.Name = "RibbonStatusBar"
+        Me.RibbonStatusBar.Ribbon = Me.RibbonControl
+        Me.RibbonStatusBar.Size = New System.Drawing.Size(442, 31)
+        Me.RibbonStatusBar.Visible = False
+        '
+        'DefaultLookAndFeel1
+        '
+        Me.DefaultLookAndFeel1.LookAndFeel.SkinName = "DevExpress Dark Style"
+        '
+        'TreeList1
+        '
+        Me.TreeList1.Columns.AddRange(New DevExpress.XtraTreeList.Columns.TreeListColumn() {Me.colfileName, Me.colLocal})
+        Me.TreeList1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TreeList1.Location = New System.Drawing.Point(0, 147)
+        Me.TreeList1.Name = "TreeList1"
+        Me.TreeList1.OptionsBehavior.Editable = False
+        Me.TreeList1.OptionsLayout.AddNewColumns = False
+        Me.TreeList1.OptionsSelection.EnableAppearanceFocusedCell = False
+        Me.TreeList1.OptionsSelection.EnableAppearanceFocusedRow = False
+        Me.TreeList1.OptionsView.EnableAppearanceEvenRow = True
+        Me.TreeList1.OptionsView.ShowCheckBoxes = True
+        Me.TreeList1.OptionsView.ShowColumns = False
+        Me.TreeList1.OptionsView.ShowFocusedFrame = False
+        Me.TreeList1.OptionsView.ShowIndicator = False
+        Me.TreeList1.SelectImageList = Me.ImageCollection1
+        Me.TreeList1.Size = New System.Drawing.Size(442, 271)
+        Me.TreeList1.TabIndex = 2
+        '
+        'colfileName
+        '
+        Me.colfileName.Caption = "Nome do Arquivo"
+        Me.colfileName.FieldName = "fileName"
+        Me.colfileName.MinWidth = 57
+        Me.colfileName.Name = "colfileName"
+        Me.colfileName.Visible = True
+        Me.colfileName.VisibleIndex = 0
+        '
+        'colLocal
+        '
+        Me.colLocal.Caption = "Local"
+        Me.colLocal.FieldName = "Local"
+        Me.colLocal.Name = "colLocal"
+        '
+        'ImageCollection1
+        '
+        Me.ImageCollection1.ImageSize = New System.Drawing.Size(24, 24)
+        Me.ImageCollection1.ImageStream = CType(resources.GetObject("ImageCollection1.ImageStream"), DevExpress.Utils.ImageCollectionStreamer)
+        Me.ImageCollection1.Images.SetKeyName(0, "opened_folder-24.png")
+        Me.ImageCollection1.Images.SetKeyName(1, "file-24.png")
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer
+        Me.FolderBrowserDialog1.ShowNewFolderButton = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(442, 449)
+        Me.Controls.Add(Me.TreeList1)
+        Me.Controls.Add(Me.RibbonStatusBar)
+        Me.Controls.Add(Me.RibbonControl)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmMain"
+        Me.Ribbon = Me.RibbonControl
+        Me.StatusBar = Me.RibbonStatusBar
+        Me.Text = "Magnemite"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.RibbonControl, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TreeList1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ImageCollection1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents RibbonControl As DevExpress.XtraBars.Ribbon.RibbonControl
+    Friend WithEvents RibbonPage1 As DevExpress.XtraBars.Ribbon.RibbonPage
+    Friend WithEvents RibbonPageGroup1 As DevExpress.XtraBars.Ribbon.RibbonPageGroup
+    Friend WithEvents RibbonStatusBar As DevExpress.XtraBars.Ribbon.RibbonStatusBar
+    Friend WithEvents DefaultLookAndFeel1 As DevExpress.LookAndFeel.DefaultLookAndFeel
+    Friend WithEvents btnSeleioncaDir As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents TreeList1 As DevExpress.XtraTreeList.TreeList
+    Friend WithEvents colfileName As DevExpress.XtraTreeList.Columns.TreeListColumn
+    Friend WithEvents colLocal As DevExpress.XtraTreeList.Columns.TreeListColumn
+    Friend WithEvents ImageCollection1 As DevExpress.Utils.ImageCollection
+    Friend WithEvents btnVerifica As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents RibbonPageGroup2 As DevExpress.XtraBars.Ribbon.RibbonPageGroup
+    Friend WithEvents btnRegistro As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents btnExcluir As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+
+
+End Class
---
+++ Magnemite/frmMain.vb
@@ -0,0 +1,120 @@
+﻿Imports DevExpress.XtraTreeList.Nodes
+Imports Microsoft.Win32.Registry
+
+Public Class frmMain
+    Private _localBase As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For Each cadaLinha As String In My.Application.CommandLineArgs.ToArray
+            montaTela(cadaLinha)
+        Next
+    End Sub
+
+    Private Sub montaTela(localBase As String)
+        _localBase = localBase
+
+        TreeList1.Nodes.Clear()
+        If System.IO.Directory.Exists(localBase) Then
+            Dim lala As New System.IO.DirectoryInfo(localBase)
+
+            Dim noPai As TreeListNode = TreeList1.AppendNode({lala.Name, lala}, DirectCast(Nothing, TreeListNode))
+            noPai.ImageIndex = 0
+
+            adicionaFilhos(lala, noPai)
+
+            TreeList1.ExpandAll()
+            btnVerifica_ItemClick(Nothing, Nothing)
+        End If
+    End Sub
+
+    Private Sub adicionaFilhos(Diretorio As System.IO.DirectoryInfo, ByRef noPai As TreeListNode)
+        For Each cadaDir As System.IO.DirectoryInfo In Diretorio.GetDirectories
+            Dim novoPai As TreeListNode = TreeList1.AppendNode({cadaDir.Name, ""}, noPai)
+            novoPai.ImageIndex = 0
+            adicionaFilhos(cadaDir, novoPai)
+        Next
+
+        For Each cadaFile As System.IO.FileInfo In Diretorio.GetFiles
+            Dim novoNo As TreeListNode = TreeList1.AppendNode({cadaFile.Name, cadaFile.FullName}, noPai)
+            novoNo.ImageIndex = 1
+            novoNo.SelectImageIndex = 1
+        Next
+    End Sub
+
+    Private Sub btnSeleioncaDir_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles btnSeleioncaDir.ItemClick
+        If FolderBrowserDialog1.ShowDialog Then
+            montaTela(FolderBrowserDialog1.SelectedPath)
+        End If
+    End Sub
+
+    Private Sub btnVerifica_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles btnVerifica.ItemClick
+        TreeList1.Nodes(0).Checked = False
+        verificaFilhos(TreeList1.Nodes(0))
+    End Sub
+
+    Private Sub verificaFilhos(noPai As TreeListNode)
+        For Each cadaNo As TreeListNode In noPai.Nodes
+            cadaNo.Checked = False
+            If cadaNo.ImageIndex = 1 Then
+                Dim arquivo As New System.IO.FileInfo(cadaNo.GetValue(colLocal))
+                If arquivo.Extension.ToUpper.Equals(".LOG") Or _
+                   arquivo.Name.ToUpper.StartsWith("ERRORS.LST.") Or _
+                   arquivo.Name.ToUpper.Contains(".INF.") Then
+                    cadaNo.Checked = True
+                End If
+                For Each cadaTipo As String In {".ASM.", ".PRT.", ".DRW."}
+                    If arquivo.Name.ToUpper.Contains(cadaTipo) Then
+                        Dim num As Integer = arquivo.Extension.Replace(".", "")
+                        Dim novoNome As String = arquivo.FullName.Substring(0, arquivo.FullName.LastIndexOf(".") + 1) & (num + 1)
+                        If System.IO.File.Exists(novoNome) Then
+                            cadaNo.Checked = True
+                        End If
+                    End If
+                Next
+            End If
+            verificaFilhos(cadaNo)
+        Next
+    End Sub
+
+   
+    Private Sub btnExcluir_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles btnExcluir.ItemClick
+        excluiItens(TreeList1.Nodes(0))
+        montaTela(_localBase)
+    End Sub
+
+    Private Sub excluiItens(noPai As TreeListNode)
+        For Each cadaNo As TreeListNode In noPai.Nodes
+            If cadaNo.ImageIndex = 1 Then
+                If cadaNo.Checked Then
+                    System.IO.File.Delete(cadaNo.GetValue(colLocal))
+                End If
+            End If
+            excluiItens(cadaNo)
+        Next
+    End Sub
+
+    Private Sub btnRegistro_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles btnRegistro.ItemClick
+        Dim appData As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+        If Not System.IO.Directory.Exists(appData & "\Magnemite\") Then
+            System.IO.Directory.CreateDirectory(appData & "\Magnemite\")
+        End If
+        Dim dirPai As New System.IO.DirectoryInfo("\\brsum-fs01\Company\BU - Comum\Publisher\Magnemite\Application Files\Magnemite_1_0_0_0\")
+        For Each cadaArquivo As System.IO.FileInfo In dirPai.GetFiles("*.dll")
+            FileCopy(cadaArquivo.FullName, appData & "\Magnemite\" & cadaArquivo.Name)
+        Next
+
+        For Each cadaArquivo As System.IO.FileInfo In dirPai.GetFiles("Magnemite.*")
+            FileCopy(cadaArquivo.FullName, appData & "\Magnemite\" & cadaArquivo.Name)
+        Next
+
+        Dim reg As Microsoft.Win32.RegistryKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey("Directory\shell\Magnemite")
+        If reg Is Nothing Then
+            reg = Microsoft.Win32.Registry.ClassesRoot.CreateSubKey("Directory\shell\Magnemite")
+            reg.SetValue("Icon", appData & "\Magnemite\magnemite.ico", Microsoft.Win32.RegistryValueKind.String)
+            reg.SetValue("", "Limpar arquivos com Magnemite")
+            reg = reg.CreateSubKey("command")
+            reg.SetValue("", appData & "\Magnemite\magnemite.exe ""%1""")
+            reg.Close()
+        End If
+    End Sub
+End Class
